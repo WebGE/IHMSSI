938f5a6ed9834a0d43b843f3fc7553380bf8dba3
Mariano Philippe <philippemariano@hotmail.fr>
2016-08-25 17:31:17 +0200

    Intégration d'un programme de test dans le projet

---
+++ NetduinoIHMSSI/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+using ToolBoxes;
+
+namespace NetduinoIHMSSI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            IHMSSI Ihm = new IHMSSI();
+
+            Ihm.Lcd.PutString(0, 0, "Classe IHMSSI");
+            Ihm.Lcd.PutString(0, 1, "Test des Leds");
+
+            // Leds test
+            Ihm.LedsWrite(0x00);
+            Thread.Sleep(1000);
+            Ihm.LedsOff();
+            Ihm.LedsWrite(0xAA);
+            Thread.Sleep(1000);
+            Ihm.LedsOff();
+            Thread.Sleep(1000);
+            Ihm.D2.Write(true);
+            Ihm.D3.Write(true);
+            Ihm.D4.Write(true);
+            Ihm.D5.Write(true);
+            Thread.Sleep(1000);
+            Ihm.LedsOff();
+            Thread.Sleep(1000);
+
+            // BPs test
+            Ihm.Lcd.ClearScreen();
+            Ihm.Lcd.PutString(0, 0, "Classe IHMSSI");
+            Ihm.Lcd.PutString(0, 1, "Test des Bps");
+
+            while (true)
+            {
+                if (Ihm.BpPlus.Read() == true)
+                    Ihm.D0.Write(true);
+                else
+                    Ihm.D0.Write(false);
+
+                if (Ihm.BpFleHaut.Read() == true)
+                    Ihm.D1.Write(true);
+                else
+                    Ihm.D1.Write(false);
+
+                if (Ihm.BpMoins.Read() == true)
+                    Ihm.D2.Write(true);
+                else
+                    Ihm.D2.Write(false);
+
+                if (Ihm.BpFleBas.Read() == true)
+                    Ihm.D3.Write(true);
+                else
+                    Ihm.D3.Write(false);
+
+                if (Ihm.BpSet.Read() == true)
+                    Ihm.D4.Write(true);
+                else
+                    Ihm.D4.Write(false);
+
+                if (Ihm.BpOk.Read() == true)
+                    Ihm.D5.Write(true);
+                else
+                    Ihm.D5.Write(false);
+
+                if (Ihm.BpEnter.Read() == true)
+                    Ihm.D6.Write(true);
+                else
+                    Ihm.D6.Write(false);
+
+                if (Ihm.BpEchap.Read() == true)
+                    Ihm.D7.Write(true);
+                else
+                    Ihm.D7.Write(false);
+                Thread.Sleep(10);
+
+            }
+
+
+        }
+
+    }
+}
